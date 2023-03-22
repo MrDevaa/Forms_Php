da19bcd971cd1983cc3b40f7cc2e37e6e21e4439
MrDevaa <d.codandabany@gmail.com>
2023-03-23 00:56:08 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import './App.css';
+import './components/ListUser';
+import ListUser from './components/ListUser';
+import CreateUser from './components/CreateUser';
+import EditUser from './components/EditUser';
 
 function App() {
   return (
     <div className="App">
-      Hello world !
+      <h5>React CRUD operations using PHP api and MySQL</h5>
+
+      <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to ="/">List Users</Link>
+          </li>
+          <li>
+            <Link to ="user/create">Create User</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route index element={<ListUser />} />
+        <Route path="user/create" element={<CreateUser />} /> 
+        <Route path="user/:id/edit" element={<EditUser />} /> 
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
